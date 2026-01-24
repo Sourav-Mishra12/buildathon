6181e24596c8ec56bf38c3a8461188dbd81a967f
sourav mishra <cag.souravmishra@gmail.com>
2026-01-24 18:37:32 +0530

    added some polishing to the app

--- backend/services/llm.py
+++ backend/services/llm.py
@@ -1,29 +1,29 @@
 
 # if using ollama local model
 
-import ollama
+# import ollama
 
-MODEL_NAME = "llama3:8b"
+# MODEL_NAME = "llama3:8b"
 
-def call_llm(prompt: str) -> str:
-    response = ollama.chat(
-        model=MODEL_NAME,
-        messages=[
-            {"role": "user", "content": prompt}
-        ]
-    )
-    return response["message"]["content"]
+# def call_llm(prompt: str) -> str:
+#     response = ollama.chat(
+#         model=MODEL_NAME,
+#         messages=[
+#             {"role": "user", "content": prompt}
+#         ]
+#     )
+#     return response["message"]["content"]
 
 # if using GROQ API
 
-# from groq import Groq
-# import os
+from groq import Groq
+import os
 
-# client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-# def call_llm(prompt: str) -> str:
-#     chat = client.chat.completions.create(
-#         model="llama3-8b-8192",
-#         messages=[{"role": "user", "content": prompt}]
-#     )
-#     return chat.choices[0].message.content
+def call_llm(prompt: str) -> str:
+    chat = client.chat.completions.create(
+        model="llama3-8b-8192",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return chat.choices[0].message.content
